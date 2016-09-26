c1ef820f67c79696b5658a54bba43dc13a31351d
jayalakshmipv <jayalakshmivjvj@gmail.com>
2016-09-26 18:09:50 +0530

    notice

--- resources/views/protected/standardUser/notice.blade.php
+++ resources/views/protected/standardUser/notice.blade.php
@@ -11,26 +11,15 @@
 			<div class="col-md-12 col-sm-22 col-xs-12 full_profile1 ">
 			
 			<div class="col-md-12 col-sm-12 col-xs-12 full_notics">
+                            @foreach( $allNotice as $notice )
 			<div class="notic_data">
 			<i class="fa  fa-chevron-right" style="font-size:18"></i>
-			&nbsp;&nbsp; Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-			when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-			when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-			<br><div class="date_noties">18/09/2016 </div>
+			&nbsp;&nbsp; {!! $notice->message !!} 
+			<br><div class="date_noties">{{ date('d-m-Y', strtotime($notice->created_at)) }} </div>
 			<div class="line1"></div>
 			</div>
+                            @endforeach
 			<br>
-			<div class="notic_data">
-			<i class="fa  fa-chevron-right" style="font-size:18"></i>
-			&nbsp;&nbsp; Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-			when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-			<br><div class="date_noties">18/09/2016 </div>
-			<div class="line1"></div>
-			</div>
-			
-			
-			
-			
 			</div>
 			</div>
 		</div>
