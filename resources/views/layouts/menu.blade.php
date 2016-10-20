5f3a49dd9667df5d24f990941eea858fe73daaee
sandrasraj <sandra@imrokraft.com>
2016-10-20 16:21:29 -0600

    route

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -141,6 +141,19 @@
                    
                 </ul>
             </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-user"></i>
+                    <span>ProgressCard</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="{{URL::route('Faculty.create')}}"><i class="fa fa-circle-o"></i> Progresscard</a></li>
+                   
+                </ul>
+            </li>
 
             <li class="treeview">
                 <a href="#">
